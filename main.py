4ce7dfd0274e34aa69f92e9300e9b8f2634a8850
99r0hit <91751811+99r0hit@users.noreply.github.com>
2025-07-27 22:23:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def create_user(req: CreateUserRequest):
 
     auth_url = f"{url}/auth/v1/admin/users"
     headers = {
-        "apikey": SUPABASE_SERVICE_ROLE,
+        "apikey": kay,
         "Authorization": f"Bearer {key}",
         "Content-Type": "application/json",
     }
