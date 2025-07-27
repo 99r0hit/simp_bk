767ed3b4bb98259bbf0d7a0d4fd863769ff0c376
99r0hit <91751811+99r0hit@users.noreply.github.com>
2025-07-27 17:03:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow React frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Later set this to your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI backend!"}
+
+@app.get("/semiconductor-info")
+def get_info():
+    return {
+        "text": "Semiconductors are the foundation of modern electronics and computing."
+    }
